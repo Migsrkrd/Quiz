d8bb58f4a12906e1a8cff16d37af1ea3d72f4910
Mike <mreickcastillero@gmail.com>
2023-09-28 21:29:29 -0700

    added initial files and started html and css

---
+++ javascript.js
@@ -0,0 +1,37 @@
+//var declaration/document linking
+    //var time remaining
+    //
+
+//make array of objects (object = question: "", answer: "", wrong answers:"")
+
+//set up function for START QUIZ button
+
+//button.eventListener("click", gamestart) ?
+
+//listen for button start
+
+//first question and answer should be display now, as well as timer is set
+
+//add eventListeners to answer options and check for true or false
+    //use if right, move onto function that goes to next question
+    //use if wrong, move onto function that goes to next question as well as decrement time
+
+//write a function for wrong and decrement timer by 10 seconds
+
+//use event listeners and once an answer is chosen, go to next question regardless 
+//(set this process by setting an interval of the object array containing questions answers and wrong answers)
+//kinda like the carrousel box example
+
+//make a total of 6 questions
+    //if last question or timer end, finish quiz and bring up last page
+    
+
+//repeat unntil timer hits 0 or all questions are all done
+    //when our array of objects returns as undefined or length-1 for index
+
+//endgame
+    //stop timer and keep stopped time displayed
+    //disaply the score at the end, which is the amount of time left on the timer
+    //prompt for initials
+    //save score and initials as (initials: score)
+
