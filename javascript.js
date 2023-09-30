152da60a4f775ea977d9e0651a9bf008866f9a4e
Mike <mreickcastillero@gmail.com>
2023-09-29 20:51:47 -0700

    finished all questions and now creating end game page with completing timeinterval if statement next

--- javascript.js
+++ javascript.js
@@ -37,58 +37,12 @@
 
 //--------------------------------------------------------------------------------------------------------------------------------
 
-// var sectionElementsArray = [questionOne = {
-//     "question": "What is Michael's favorite color?",
-//     "true answer": "green",
-//     "falseAnswer1": "red",
-//     "falseAnswer2": "blue",
-//     "falseAnswer3": "yellow"
-//     },
-
-//     questionTwo = {
-//     "question": "How old is Michael?(Hint: he was born in 2000)",
-//     "true answer": "25",
-//     "falseAnswer1": "23",
-//     "falseAnswer2": "21",
-//     "falseAnswer3": "27"
-//     },
-
-//     questionThree = {
-//     "question": "Does Michael ever lie?",
-//     "true answer": "yep, he just did",
-//     "falseAnswer1": "never in a million years",
-//     },
-
-//     questionFour = {
-//     "question": "Does Michael deserve an A on this challenge?",
-//     "true answer": "yes, he is the greatest student I have ever seen",
-//     "falseAnswer1": "No, but I have poor judgement",
-//     "falseAnswer2": "No, I am giving him a lower grade because I'm secretly jealous of him",
-//     "falseAnswer3": "idk maybe"
-//     },
-
-//     questionFive = {
-//     "question": "THIS IS THE LAST QUESTION: How did I do?",
-//     "true answer": "Amazing",
-//     "falseAnswer1": "Amazing but with jazz hands",
-//     "falseAnswer2": "Greatest quiz I have ever seen",
-//     "falseAnswer3": "Meh",
-//     },
-
-//     questionSix = {
-//     "question": "Did Michael lie again?",
-//     "true answer": "yep, now you get an F",
-//     "falseAnswer1": "Lie? does Michael ever lie?",
-//     "falseAnswer2": "maybe ;)",
-//     "falseAnswer3": "purple"
-//     }]
-
     var headerTag = document.querySelector("header");
     var quizStartButton = document.querySelector("#quizStarter");
     var questionBox = document.getElementById("questionBox");
     var answerBox = document.getElementById("answerBox");
     var timeElement = document.querySelector(".time");
-    var secondsLeft = 120;
+    var secondsLeft = 180;
     var answerBox1 = document.getElementById("answerBox1");
     var answerBox2 = document.getElementById("answerBox2");
     var answerBox3 = document.getElementById("answerBox3");
@@ -96,7 +50,7 @@
     var answer1 = document.querySelector(".answer1");
     var answer2 = document.querySelector(".answer2");
     var answer3 = document.querySelector(".answer3");
-    var buttonsArray = [answerBox, answerBox1, answerBox2, answerBox3]
+    var buttonsArray = [answerBox, answerBox1, answerBox2, answerBox3];
   
 
 
@@ -104,11 +58,14 @@
        var timerInterval = setInterval(function(){
             secondsLeft -- ;
             timeElement.textContent = secondsLeft;
-            if(secondsLeft === 0) {
+            if(secondsLeft <= 0) {
                 clearInterval(timerInterval);
                 //place it here!!!!!
             }
         }, 1000);
+            if(endGame()===true){
+                clearInterval(timerInterval);
+            }
     }
 
 // add function for what happens if timer runs out here and place above
@@ -120,40 +77,147 @@
         quizStartButton.setAttribute("style", "display: none");
         questionBox.textContent = "What is Michael's favorite color?";
         answerBox.innerHTML = "<button>A</button>";
-        answerBox.addEventListener("click", next);
+        answerBox.addEventListener("click", question2);
         answerBox1.innerHTML = "<button>B</button>";
         answerBox1.addEventListener("click", function(){
-            secondsLeft=secondsLeft-15;
-            next();
+            secondsLeft = secondsLeft-15;
+            question2();
         });
         answerBox2.innerHTML = "<button>C</button>";
-        answerBox2.addEventListener("click", function(){                //perfect repeatable here <--
+        answerBox2.addEventListener("click", function(){
             secondsLeft=secondsLeft-15;
-            next();
+            question2();
         });
         answerBox3.innerHTML = "<button>D</button>";
         answerBox3.addEventListener("click", function(){
             secondsLeft=secondsLeft-15;
-            next();
+            question2();
         });
         answer.textContent = "green";
         answer1.textContent = "blue";
         answer2.textContent = "red";
         answer3.textContent = "yellow";
-
-        // answerBox.onclick = questionBox.textContent = "Nice!"
     });
 
-    function next(){
+    function question2(){
         questionBox.textContent = "How old is Michael?(Hint: he was born in 2000)";
-        answerBox.innerHTML = "<button>A</button>";
-        answerBox.addEventListener("click", next);
+        answerBox.addEventListener("click", function(){
+            secondsLeft = secondsLeft-15
+            question3();
+        });
+        answerBox2.addEventListener("click", function(){
+            secondsLeft = secondsLeft+15;
+            question3();
+        });answerBox1.addEventListener("click", function(){
+            question3();
+        });answerBox3.addEventListener("click", function(){
+            question3();
+        });
         answer.textContent = "23";
         answer1.textContent = "24";
         answer2.textContent = "25";
         answer3.textContent = "26";
     }
     
+    function question3(){
+        questionBox.textContent = "Does Michael ever lie?";
+        answerBox.addEventListener("click", function(){
+            secondsLeft = secondsLeft+30;
+            question4();
+        });
+        answerBox1.addEventListener("click", function(){
+            question4();
+        });
+        answerBox2.addEventListener("click", function(){
+            secondsLeft = secondsLeft-30;
+            question4();
+        });
+       answerBox3.addEventListener("click", function(){
+            question4();
+       });
+        answer.textContent = "yep, he just did";
+        answer1.textContent = "I was too lazy to convert to two answers so pls dont pick me";
+        answer2.textContent = "never in a million years";
+        answer3.textContent = "I wasn't kidding when I said I was too lazy";
+        }
+
+
+    function question4(){
+        questionBox.textContent = "Does Michael deserve an A on this challenge?";
+        answerBox.addEventListener("click", function(){
+            secondsLeft = secondsLeft-60;
+            question5();
+        });
+        answerBox1.addEventListener("click", function(){
+            secondsLeft = secondsLeft+15;
+            question5();
+        });
+        answerBox2.addEventListener("click", function(){
+            secondsLeft = secondsLeft+45;
+            question5();
+        });
+       answerBox3.addEventListener("click", function(){
+            question5();
+        });
+        answer.textContent = "No, but I have poor judgement";
+        answer1.textContent = "yes, he is the greatest student I have ever seen";
+        answer2.textContent = "No, I am giving him a lower grade because I'm secretly jealous of him";
+        answer3.textContent = "idk maybe";
+    }
+
+    function question5(){
+        questionBox.textContent = "THIS IS THE LAST QUESTION: How did I do?";
+        answerBox.addEventListener("click", function(){
+            secondsLeft = secondsLeft+105;
+            question6();
+        });
+        answerBox1.addEventListener("click", function(){
+            secondsLeft = secondsLeft-60;
+            question6();
+        });
+        answerBox2.addEventListener("click", function(){
+            secondsLeft = secondsLeft-60;
+            question6();
+        });
+       answerBox3.addEventListener("click", function(){
+            secondsLeft = secondsLeft+15;
+            question6();
+        });
+        answer.textContent = "Amazing but with jazz hands";
+        answer1.textContent = "Meh";
+        answer2.textContent = "Amazing but with jazz hands part 2";
+        answer3.textContent = "Amazing (This is the right answer)";
+    }
+
+    function question6(){
+        questionBox.textContent = "Did Michael lie again?";
+        answerBox.addEventListener("click", function(){
+            secondsLeft = secondsLeft-165;
+            endGame();
+        });
+        answerBox1.addEventListener("click", function(){
+            secondsLeft = secondsLeft+150;
+            endGame();
+        });
+        answerBox2.addEventListener("click", function(){
+            secondsLeft = secondsLeft+75;
+            endGame();
+        });
+        answerBox3.addEventListener("click", function(){
+            secondsLeft = secondsLeft-60;
+            endGame();
+        });
+        answer.textContent = "purple";
+        answer1.textContent = "maybe ;)";
+        answer2.textContent = "Lie? does Michael ever lie?";
+        answer3.textContent = "yep, now you get an F";
+    }
+
+    function endGame (){
+        document.body.children[0].children[0].textContent = secondsLeft;
+    }
 
 
 
+    
+    
